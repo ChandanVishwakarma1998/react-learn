e0257b2e829e91ecb903fb91191389d8df56e0e6
bbbbbbbb123 <122397079+bbbbbbbb123@users.noreply.github.com>
2024-08-18 01:41:43 +0530

    random color generator

---
+++ RandomColorGenerator/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState , useEffect } from 'react'
+import './style.css'
+import './App.css'
+
+function App() {
+   const [typeOfColor, setTypeOfColor] = useState('HEX')
+   const [color, setColor] = useState('#000000') 
+
+   const randomColorUtility = (length) => (
+  Math.floor(Math.random() * length)
+   )
+
+ const handleRandomHexColor = () => {
+  const hex =[1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+  let hexColor = '#'
+  for (let i =0; i < 6; i++) {
+    hexColor += hex[randomColorUtility(hex.length)]
+    console.log(hexColor);
+    
+   }
+   setColor(hexColor)
+ } 
+  
+ const  handleRandomRgbColor = () => {
+  const r = randomColorUtility(256);
+  const g = randomColorUtility(256);
+  const b = randomColorUtility(256);
+
+  setColor(`rgb(${r},${g},${b})`)
+ } 
+ useEffect(() => {
+  (typeOfColor === "hex" ? handleRandomHexColor : handleRandomRgbColor)
+ }, [typeOfColor])
+ 
+     
+  return (
+    <div style={{
+      width:"100vw",
+      height: "100vh",
+      background: color ,
+      }}>
+        <div className='main'>
+        <button onClick={()=> setTypeOfColor('hex')}>Create random HEX Color </button>
+      <button onClick={()=>setTypeOfColor("rgb")}>Create random RGB Color </button>
+      </div>
+      <button onClick={typeOfColor === "hex" ? handleRandomHexColor:handleRandomRgbColor}>Create random Color </button>
+    
+      <div
+       style={{
+        display:"flex",
+        flexDirection: 'column',
+        justifyContent:'center',
+        alignItems : 'center',
+        color:"#fff",
+        fontSize: "40px",
+        fontWeight: "30px",
+        marginTop:"50px",
+        gap: "20px",
+        }}> 
+        <h2>{typeOfColor === "rgb"  ? " RGB Color":"HEX Color"}
+        </h2>
+        <h3>{color}</h3>
+        </div>
+      
+    </div>
+   )
+}
+
+export default App
